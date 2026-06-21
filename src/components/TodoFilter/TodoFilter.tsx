6533703620e45aacb208707b007825377c5cb972
Mikitjuk Pavlo <mikitjukpavlo@mail.com>
2026-06-21 10:22:03 +0300

    solution clear all

--- src/components/TodoFilter/TodoFilter.tsx
+++ src/components/TodoFilter/TodoFilter.tsx
@@ -10,46 +10,54 @@ export const TodoFilter: React.FC<Props> = ({
   setQuery,
   statusFilter,
   setStatusFilter,
-}) => (
-  <form className="field has-addons">
-    <p className="control">
-      <span className="select">
-        <select
-          value={statusFilter}
-          onChange={event => setStatusFilter(event.target.value)}
-          data-cy="statusSelect"
-        >
-          <option value="all">All</option>
-          <option value="active">Active</option>
-          <option value="completed">Completed</option>
-        </select>
-      </span>
-    </p>
+}) => {
+  // Функція для повного скидання фільтрів
+  const handleClear = () => {
+    setQuery('');
+    setStatusFilter('all'); // Скидаємо фільтр статусу до "All"
+  };
 
-    <p className="control is-expanded has-icons-left has-icons-right">
-      <input
-        data-cy="searchInput"
-        value={query}
-        onChange={event => setQuery(event.target.value)}
-        type="text"
-        className="input"
-        placeholder="Search..."
-      />
-      <span className="icon is-left">
-        <i className="fas fa-magnifying-glass" />
-      </span>
-
-      {query && (
-        <span className="icon is-right" style={{ pointerEvents: 'all' }}>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <button
-            data-cy="clearSearchButton"
-            type="button"
-            className="delete"
-            onClick={() => setQuery('')}
-          />
+  return (
+    <form className="field has-addons">
+      <p className="control">
+        <span className="select">
+          <select
+            value={statusFilter}
+            onChange={event => setStatusFilter(event.target.value)}
+            data-cy="statusSelect"
+          >
+            <option value="all">All</option>
+            <option value="active">Active</option>
+            <option value="completed">Completed</option>
+          </select>
         </span>
-      )}
-    </p>
-  </form>
-);
+      </p>
+
+      <p className="control is-expanded has-icons-left has-icons-right">
+        <input
+          data-cy="searchInput"
+          value={query}
+          onChange={event => setQuery(event.target.value)}
+          type="text"
+          className="input"
+          placeholder="Search..."
+        />
+        <span className="icon is-left">
+          <i className="fas fa-magnifying-glass" />
+        </span>
+
+        {query && (
+          <span className="icon is-right" style={{ pointerEvents: 'all' }}>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <button
+              data-cy="clearSearchButton"
+              type="button"
+              className="delete"
+              onClick={handleClear} // Викликаємо функцію скидання
+            />
+          </span>
+        )}
+      </p>
+    </form>
+  );
+};
